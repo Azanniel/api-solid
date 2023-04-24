b5da4d59b4493be06fbaf8285c3ea1210d17beae
azanniel <leo.azannielttt@gmail.com>
2023-04-24 19:31:02 -0400

    ✨ add authenticate feature

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -12,11 +12,6 @@ interface AuthenticateUseCaseResponse {
   user: User
 }
 
-/**
- * 1 - buscar o usuário no banco de dados pelo e-mail
- * 2 - comparar se a senha salva no banco de dados bate com a senha vinda do parâmetro
- */
-
 export class AuthenticateUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
