7c103d250aa9b23034807623c064f4e0a6577f80
azanniel <leo.azannielttt@gmail.com>
2023-05-29 15:27:49 -0400

    🧐 create end to end tests for register and authenticate

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,7 +1,23 @@
-import { describe, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-describe('End to end test', () => {
-  it('a test environment', () => {
-    console.log('Testing')
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
   })
 })
